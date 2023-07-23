cc8314b9c8ef694251b72da96f99ac40da4a2c51
jens2501 <johndoe@example.comjens.saberi@gmail.com>
2023-07-23 13:53:37 +0200

    Take a phot code inserted

--- First_Camera_Tests.py
+++ First_Camera_Tests.py
@@ -1,1 +1,12 @@
 from picamera import PiCamera
+import time
+
+camera = PiCamera()
+camera.resolution = (1280, 720)
+camera.rotation = 180
+time.sleep(2)
+
+file_name = "/home/pi/camera/IMG_Test_GitHub.jpg"
+camera.capture(file_name)
+
+print("Done")
